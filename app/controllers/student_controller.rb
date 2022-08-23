2ec606fcc808915fc047d906fa133963dc627154
Sooraj Philip <soorajphilip97@gmail.com>
2022-08-23 22:45:48 +0530

    Add profile page and html clean up in navbar and sidebar

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,4 +1,7 @@
 class StudentController < ApplicationController
   def index
   end
+
+  def profile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'student#index'
+  get '/s/dashboard', to: 'student#index'
+  get '/s/my-profile', to: 'student#profile'
 end
