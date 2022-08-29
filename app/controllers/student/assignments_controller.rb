98926031a77213c2204a07b963623fe46cecaf4c
Sooraj Philip <soorajphilip97@gmail.com>
2022-08-29 14:07:49 +0530

    Update assignments page

--- app/controllers/student/assignments_controller.rb
+++ app/controllers/student/assignments_controller.rb
@@ -1,6 +1,13 @@
 class Student::AssignmentsController < ApplicationController
   layout 'student'
+  before_action :set_current_user
 
   def index
+  end
+
+  private
+
+  def set_current_user
+    @user = current_user
   end
 end
